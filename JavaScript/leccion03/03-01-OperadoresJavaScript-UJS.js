dc75e20cf0fab3bffa2492d5557bbb18588a3de5
Jarl <jhosepandrey203@gmail.com>
2024-02-03 12:05:21 -0500

    Operadores de incremento y decremento en JavaScript

--- JavaScript/leccion03/03-01-OperadoresJavaScript-UJS.js
+++ JavaScript/leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -1,19 +1,24 @@
 let a = 3, b = 2;
 let z = a + b;
-console.log("Resultado de la suma: " +  z);
 
-z = a - b;
-console.log("Resultado de la resta: " + z);
+// Incremento
+// Pre-incremento (el operador ++ antes de la variable)
+z = ++a;
+console.log(a);
+console.log(z);
 
-z = a * b;
-console.log("Resultado de la mult; " + z);
+// Post-incremento (el operador ++ despues de la variable)
+z = b++;
+console.log(b);
+console.log(z);
 
-z = a / b;
-console.log("Resultado de la división; " + z);
+// Decremento
+// Predecremento
+z = --a;
+console.log(a);
+console.log(z);
 
-z = a % b;
-console.log("Resultado de operación modulo (residuo): " + z);
-
-z = a ** b;
-console.log("Resultado de operador exponente: " + z);
-
+// Postdecremento
+z = b--;
+console.log(b);
+console.log(z);
