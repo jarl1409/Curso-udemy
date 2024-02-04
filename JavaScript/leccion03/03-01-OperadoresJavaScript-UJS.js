f07c4474aa317f09d977bd1ed43cc451fa2d8f4d
Jarl <jhosepandrey203@gmail.com>
2024-02-04 17:28:43 -0500

    Operador OR en JavaScript

--- JavaScript/leccion03/03-01-OperadoresJavaScript-UJS.js
+++ JavaScript/leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -1,3 +1,4 @@
+// Ejemplo de AND (&&), regresa tru solo si ambos operandos son true
 let a = 5;
 let ValMin= 0, ValMax = 10;
 
@@ -6,4 +7,13 @@ if ( a >= ValMin && a <= ValMax){
 }
 else{
     console.log("Fuera de rango");
+}
+// Ejemplo de OR (||), regresa true si cualquier operando es true
+let vacaciones = false, diaDescanso = true;
+
+if( vacaciones || diaDescanso){
+    console.log("Padre puede asistir al juego del hijo");
+}
+else{
+    console.log("El padre esta ocupado");
 }
