d60f6138ff9d45609dc5056128edbb04525ca83f
Jarl <jhosepandrey203@gmail.com>
2024-02-04 17:55:31 -0500

    Operador Ternario en JavaScript

--- JavaScript/leccion03/03-01-OperadoresJavaScript-UJS.js
+++ JavaScript/leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -1,19 +1,6 @@
-// Ejemplo de AND (&&), regresa tru solo si ambos operandos son true
-let a = 5;
-let ValMin= 0, ValMax = 10;
+let resultado = (3>2) ? "Verdadero" : "Falso";
+console.log(resultado);
 
-if ( a >= ValMin && a <= ValMax){
-    console.log( "Dentro del rango");
-}
-else{
-    console.log("Fuera de rango");
-}
-// Ejemplo de OR (||), regresa true si cualquier operando es true
-let vacaciones = false, diaDescanso = true;
-
-if( vacaciones || diaDescanso){
-    console.log("Padre puede asistir al juego del hijo");
-}
-else{
-    console.log("El padre esta ocupado");
-}
+let numero = 9;
+resultado = (numero % 2 == 0) ? "Número par" : "Número impar";
+console.log(resultado);
