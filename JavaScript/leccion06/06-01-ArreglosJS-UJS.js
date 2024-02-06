1a2168a838e97c36fa0d0500f0abfd8cf9f1073f
Jarl <jhosepandrey203@gmail.com>
2024-02-06 18:37:56 -0500

    Modificar los Elementos de un Arreglo en JavaScript

--- JavaScript/leccion06/06-01-ArreglosJS-UJS.js
+++ JavaScript/leccion06/06-01-ArreglosJS-UJS.js
@@ -8,3 +8,9 @@ console.log(autos[2]);
 for(let i = 0; i < autos.length; i++){
     console.log( i + " : " + autos[i]);
 }
+
+autos[1] = "MercedesBenz";
+console.log(autos[1]);
+
+autos.push("Audi");
+console.log(autos);
