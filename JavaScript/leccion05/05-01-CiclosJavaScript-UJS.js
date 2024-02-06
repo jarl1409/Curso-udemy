2a659f7b9a60beda206258aeaa0191ccab2705b2
Jarl <jhosepandrey203@gmail.com>
2024-02-06 18:04:55 -0500

    Ciclo for en JavaScript

--- JavaScript/leccion05/05-01-CiclosJavaScript-UJS.js
+++ JavaScript/leccion05/05-01-CiclosJavaScript-UJS.js
@@ -1,4 +1,4 @@
-let contador = 0;
+// let contador = 0;
 
 // while (contador < 3 ){
 //     console.log(contador);
@@ -6,8 +6,14 @@ let contador = 0;
 // }
 // console.log("Fin ciclo while");
 
-do{
-    console.log(contador);
-    contador++;
-}while(contador <3);
-console.log("Fin ciclio do while");
+// do{
+//     console.log(contador);
+//     contador++;
+// }while(contador <3);
+// console.log("Fin ciclio do while");
+
+for(let contador = 0 ; contador < 3 ; contador++ ){
+    console.log(contador);   
+}
+console.log("Fin ciclio for");
+
