1ed5b94bd9ba7366e152d721bc3d5c8a6ef85840
Jarl <jhosepandrey203@gmail.com>
2024-02-06 18:09:46 -0500

    Palabra break en JavaScript

--- JavaScript/leccion05/05-01-CiclosJavaScript-UJS.js
+++ JavaScript/leccion05/05-01-CiclosJavaScript-UJS.js
@@ -1,19 +1,7 @@
-// let contador = 0;
-
-// while (contador < 3 ){
-//     console.log(contador);
-//     contador++;
-// }
-// console.log("Fin ciclo while");
-
-// do{
-//     console.log(contador);
-//     contador++;
-// }while(contador <3);
-// console.log("Fin ciclio do while");
-
-for(let contador = 0 ; contador < 3 ; contador++ ){
-    console.log(contador);   
+for(let contador = 0; contador <= 10 ; contador++ ){
+    if (contador % 2 == 0)
+    {console.log(contador);
+        break;
+    }
 }
-console.log("Fin ciclio for");
-
+console.log("fin del ciclo for")
