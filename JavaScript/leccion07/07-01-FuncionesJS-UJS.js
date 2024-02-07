38e470ed1b6ad9346250f3ca4f5e98b92e99feaa
Jarl <jhosepandrey203@gmail.com>
2024-02-06 21:31:30 -0500

    Funciones de Tipo Expresión en JavaScript

--- JavaScript/leccion07/07-01-FuncionesJS-UJS.js
+++ JavaScript/leccion07/07-01-FuncionesJS-UJS.js
@@ -1,10 +1,14 @@
-// Hoisting
-miFuncion(4, 2);
-
 // Declaración de la función
 function miFuncion( a, b ){
-    console.log("Suma; " + (a + b));
+    return a + b;
 }
 
 // Llamando a la función
-miFuncion(2, 3);
+let resultado = miFuncion(2, 3);
+console.log(resultado);
+
+// declaración de una funtion de tipo expresión
+let sumar = function(a, b){return a+ b};
+
+resultado = sumar (1, 2);
+console.log(resultado);
