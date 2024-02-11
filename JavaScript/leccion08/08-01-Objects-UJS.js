b4695c10d353359e0f3468381e37aa1d67a6c96d
Jarl <jhosepandrey203@gmail.com>
2024-02-10 23:09:18 -0500

    Objetos en JavaScript

---
+++ JavaScript/leccion08/08-01-Objects-UJS.js
@@ -0,0 +1,15 @@
+let x = 10;
+
+let persona ={
+    nombre: "Juan",
+    apellido: "Perez",
+    email: "jperez@email.com",
+    edad: 20
+}
+
+console.log(persona.nombre);
+console.log(persona.edad);
+console.log(persona.apellido);
+console.log(persona.email);
+
+console.log(persona);
