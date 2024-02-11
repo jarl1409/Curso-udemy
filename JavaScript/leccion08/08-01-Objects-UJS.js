cd8b5dd15c2536b952d6121a7599300b0a622f58
Jarl <jhosepandrey203@gmail.com>
2024-02-11 15:30:11 -0500

    Método Apply en JavaScript

--- JavaScript/leccion08/08-01-Objects-UJS.js
+++ JavaScript/leccion08/08-01-Objects-UJS.js
@@ -1,7 +1,8 @@
 let persona1 = {
     nombre : "Juan",
     apellido : "Perez",
-    nombreCompleto: function(titulo, tel){
+    nombreCompleto: function( titulo, tel){
+        // return this.nombre + " " + this.apellido;
         return titulo + " " + this.nombre + " " + this.apellido + " " + tel;
     }
 }
@@ -13,6 +14,9 @@ let persona2 = {
 
 // uso de call para usar
 // el metodo persona1.nombreCompleto con los datos del persona2
-console.log( persona1.nombreCompleto("Lic.","221312313"));
-console.log( persona1.nombreCompleto.call(persona2, "Ingeniro", "212312233") );
+console.log( persona1.nombreCompleto("Lic.", "12434123"));
+
+let arreglo = ["Ing.", "312345513"];
+console.log( persona1.nombreCompleto.apply(persona2, arreglo) );
+
 
