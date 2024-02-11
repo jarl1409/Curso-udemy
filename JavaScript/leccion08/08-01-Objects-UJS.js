43f75a4b6b9e7717893d411368a169f36072bf9a
Jarl <jhosepandrey203@gmail.com>
2024-02-11 14:49:46 -0500

    Uso de Call en JavaScript

--- JavaScript/leccion08/08-01-Objects-UJS.js
+++ JavaScript/leccion08/08-01-Objects-UJS.js
@@ -1,20 +1,18 @@
-// Funcion contructor de objetos de tipo persona
-function Persona(nombre, apellido, email){
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.email = email;
-    this.nombreCompleto = function(){
+let persona1 = {
+    nombre : "Juan",
+    apellido : "Perez",
+    nombreCompleto: function(){
         return this.nombre + " " + this.apellido
     }
 }
 
-Persona.prototype.tel = "312233123"
+let persona2 = {
+    nombre : "Carlos",
+    apellido : "Lara",
+}
 
-let padre = new Persona("Juan", "Perez", "Jpereasdas12s@mail.com");
-padre.tel = "123412312"
+// uso de call para usar
+// el metodo persona1.nombreCompleto con los datos del persona2
+console.log( persona1.nombreCompleto());
+console.log( persona1.nombreCompleto.call(persona2) );
 
-let madre = new Persona ("Laura", "Quintero", "Lquintero@email.com");
-madre.tel = "341232345"
-
-console.log(padre.tel);
-console.log(madre.tel);
