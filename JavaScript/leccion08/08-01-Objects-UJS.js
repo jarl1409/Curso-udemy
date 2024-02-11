cbdf34ee2cd835975e9dc6cc05b8c1064b411b65
Jarl <jhosepandrey203@gmail.com>
2024-02-10 23:42:52 -0500

    Método get en Objetos de JavaScript

--- JavaScript/leccion08/08-01-Objects-UJS.js
+++ JavaScript/leccion08/08-01-Objects-UJS.js
@@ -3,31 +3,9 @@ let persona ={
     apellido: "Perez",
     email: "jperez@email.com",
     edad: 20,
-    nombreCompleto: function(){
+    get nombreCompleto(){
         return this.nombre + " " + this.apellido;
     }
 }
 
-persona.tel = "33211233";
-
-console.log(persona);
-
-delete persona.tel;
-
-console.log(persona);
-
-// concatenar cada valor de cada propiedad
-console.log( persona.nombre + "," + persona.apellido);
-
-// for in
-for( nombrePropiedad in persona){
-    console.log(persona[nombrePropiedad]);
-}
-
-// Object.values
-let personaArray = Object.values ( persona );
-console.log(personaArray);
-
-// JSON.stringify
-let personaString = JSON.stringify(persona);
-console.log(personaString);
+console.log(persona.nombreCompleto);
