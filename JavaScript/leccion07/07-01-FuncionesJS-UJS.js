0c16f0ed3c0034517cfdd9dca15c42cae48605e5
Jarl <jhosepandrey203@gmail.com>
2024-02-10 22:59:51 -0500

    Paso por Referencia en Funciones de JavaScript

--- JavaScript/leccion07/07-01-FuncionesJS-UJS.js
+++ JavaScript/leccion07/07-01-FuncionesJS-UJS.js
@@ -9,4 +9,21 @@ function cambiarValor(a){
 cambiarValor (x);
 console.log(x);
 
-console.log(a);
+// console.log(a);
+
+const persona ={
+    nombre: "Juan",
+    apellido: "Perez"
+}
+console.log(persona);
+
+function cambiarValorObjeto(p1){
+    p1.nombre = "Carlos";
+    p1.apellido = "Lara";
+}
+
+// Paso por referencia
+cambiarValorObjeto(persona);
+console.log(persona);
+
+
