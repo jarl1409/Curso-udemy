dc8d1df76987eb92bbff71c73c1d884c1fd52c6a
Jarl <jhosepandrey203@gmail.com>
2024-02-11 14:30:01 -0500

    Agregar Métodos a un Método Constructor de Objetos

--- JavaScript/leccion08/08-01-Objects-UJS.js
+++ JavaScript/leccion08/08-01-Objects-UJS.js
@@ -3,13 +3,16 @@ function Persona(nombre, apellido, email){
     this.nombre = nombre;
     this.apellido = apellido;
     this.email = email;
+    this.nombreCompleto = function(){
+        return this.nombre + " " + this.apellido
+    }
 }
 
 let padre = new Persona("Juan", "Perez", "Jpereasdas12s@mail.com");
-console.log(padre);
+console.log(padre.nombreCompleto());
 
 let madre = new Persona ("Laura", "Quintero", "Lquintero@email.com");
-console.log(madre);
+console.log(madre.nombreCompleto());
 
 padre.nombre = "carlos";
 console.log(padre);
