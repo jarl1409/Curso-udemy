a2aed3c9302306a7c40307260f662c8ad7c96c5b
Jarl <jhosepandrey203@gmail.com>
2024-02-10 23:38:28 -0500

    Distintas Formas de Imprimir un Object en JavaScript

--- JavaScript/leccion08/08-01-Objects-UJS.js
+++ JavaScript/leccion08/08-01-Objects-UJS.js
@@ -15,3 +15,19 @@ console.log(persona);
 delete persona.tel;
 
 console.log(persona);
+
+// concatenar cada valor de cada propiedad
+console.log( persona.nombre + "," + persona.apellido);
+
+// for in
+for( nombrePropiedad in persona){
+    console.log(persona[nombrePropiedad]);
+}
+
+// Object.values
+let personaArray = Object.values ( persona );
+console.log(personaArray);
+
+// JSON.stringify
+let personaString = JSON.stringify(persona);
+console.log(personaString);
