26cfd4ed9df10121e133ce1c7d19dbf63bd6dc9c
Jarl <jhosepandrey203@gmail.com>
2024-02-11 14:53:46 -0500

    Paso de Argumentos a Call en JavaScript

--- JavaScript/leccion08/08-01-Objects-UJS.js
+++ JavaScript/leccion08/08-01-Objects-UJS.js
@@ -1,8 +1,8 @@
 let persona1 = {
     nombre : "Juan",
     apellido : "Perez",
-    nombreCompleto: function(){
-        return this.nombre + " " + this.apellido
+    nombreCompleto: function(titulo, tel){
+        return titulo + " " + this.nombre + " " + this.apellido + " " + tel;
     }
 }
 
@@ -13,6 +13,6 @@ let persona2 = {
 
 // uso de call para usar
 // el metodo persona1.nombreCompleto con los datos del persona2
-console.log( persona1.nombreCompleto());
-console.log( persona1.nombreCompleto.call(persona2) );
+console.log( persona1.nombreCompleto("Lic.","221312313"));
+console.log( persona1.nombreCompleto.call(persona2, "Ingeniro", "212312233") );
 
