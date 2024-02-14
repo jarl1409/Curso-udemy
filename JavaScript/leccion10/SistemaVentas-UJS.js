56d2e4f329b68681536d9e867fb2456d4e44d667
Jarl <jhosepandrey203@gmail.com>
2024-02-13 23:04:00 -0500

    Creación Clase Orden - parte 2

--- JavaScript/leccion10/SistemaVentas-UJS.js
+++ JavaScript/leccion10/SistemaVentas-UJS.js
@@ -25,9 +25,51 @@ class Producto{
         return this._precio=precio;
     }
     toString(){
-        return `Item:${this._idProducto} 
+        return `${this._idProducto} 
         Descripción: ${this._nombre} 
         Precio: $${this.precio}`;
+    }
+}
+
+class Orden{
+    static contadorOrdenes=0;
+
+    static get MAX_PRODUCTOS(){
+        return 5;
+    }
+
+    constructor(){
+        this._idOrden = ++Orden.contadorOrdenes;
+        this._productos=[];
+        this._contadorProductosAgregados= 0;
+    }
+    get idOrden(){
+        return this._idOrden;
+    }
+    agregarProducto(producto){
+        if(this._productos.length < Orden.MAX_PRODUCTOS){
+            this._productos.push(producto);
+            // this._productos[this._contadorProductosAgregados++]= producto;
+        }
+        else{
+            console.log("No se pueden agregar más productos");
+        }
+    }
+
+    calcularTotal(){
+        let totalVenta = 0;
+        for(let producto; off; this._productos){
+            totalVenta += producto.precio;//totalVenta = totalVenta + producto.precio
+        }
+    }
+
+    mostrarOrden(){
+        let productoOrden = '';
+        for( let producto of this._productos){
+            productosOrden += producto.toString()+ ' ';
+        }
+
+        console.log(`Orden: ${this._idOrden} Total: ${this.calcularTotal()}, Productos: ${productoOrden}`);
     }
 }
 
