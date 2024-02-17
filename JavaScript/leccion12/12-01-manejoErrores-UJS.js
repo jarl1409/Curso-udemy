94635bac5c618206d3e339cfc3437fa346208774
Jarl <jhosepandrey203@gmail.com>
2024-02-16 21:01:31 -0500

    Bloque try catch finally en JavaScript

---
+++ JavaScript/leccion12/12-01-manejoErrores-UJS.js
@@ -0,0 +1,14 @@
+'use strict';
+
+try{
+    let x = 10;
+}
+catch (error){
+    console.log(error);
+}
+
+finally{
+        console.log('Termina la revisión de errores');
+}
+
+console.log('Continuamos...');
