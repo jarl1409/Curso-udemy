c17b72bc45009d5c4453cf28e3d520a4c9b44db1
Jarl <jhosepandrey203@gmail.com>
2024-02-16 19:51:24 -0500

    Modo Strict en JavaScript

---
+++ JavaScript/leccion10/10-01-ModoStrict-UJS.js
@@ -0,0 +1,13 @@
+"use strict"; // Toca definir una variable para poder usarla. MODO ESTRICTO
+
+x = 10;
+console.log(x);
+
+miFuntion(); 
+
+function miFuntion(){
+    'use strict';
+    let y = 15;
+    console.log(y);
+}
+
