a927534564084f6d0585ceee364c68558f4e3414
Jarl <jhosepandrey203@gmail.com>
2024-02-19 17:27:29 -0500

    Funciones Flecha en JavaScript

---
+++ JavaScript/leccion13/13-01-FuncionesFlecha-UJS.js
@@ -0,0 +1,12 @@
+let miFuncion = function (){
+    console.log('Saludos desde mi función');
+}
+
+const miFuncionFlecha = ()=> {
+    console.log('Saludos desde mi función flecha');
+}
+
+
+
+miFuncion();
+miFuncionFlecha();
