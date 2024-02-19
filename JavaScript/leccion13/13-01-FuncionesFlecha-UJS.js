779101761fd60194b37770037755caad4846fc99
Jarl <jhosepandrey203@gmail.com>
2024-02-19 17:46:10 -0500

    [200~Más de Funciones Flecha en JavaScript~

--- JavaScript/leccion13/13-01-FuncionesFlecha-UJS.js
+++ JavaScript/leccion13/13-01-FuncionesFlecha-UJS.js
@@ -2,11 +2,37 @@ let miFuncion = function (){
     console.log('Saludos desde mi función');
 }
 
-const miFuncionFlecha = ()=> {
-    console.log('Saludos desde mi función flecha');
-}
+// const miFuncionFlecha = ()=> {
+//     console.log('Saludos desde mi función flecha');
+// }
 
-
+// const miFuncionFlecha = () => console.log('Saludos desde mi función flecha');
 
 miFuncion();
-miFuncionFlecha();
+// miFuncionFlecha();
+
+// const saludar = () => {
+//     return 'Saludos desde función flecha';
+// }
+
+const saludar = () => 'Saludos desde función flecha';
+
+console.log(saludar());
+
+const regresaObjeto = () => ({nombre: 'Juan', apellido: 'Lopez'});
+console.log(regresaObjeto());
+
+// const funcionConParametros = (mensaje) => console.log( mensaje );
+const funcionConParametros = mensaje => console.log( mensaje );
+funcionConParametros ('Saludos con parametros')
+
+const funcionConVariosParametrosClasico = (op1, op2) => {
+    let resultado = op1 + op2;
+    return `Resultado: ${resultado}`;
+}
+
+console.log( funcionConVariosParametrosClasico(5,5));
+
+
+const funcionConVariosParametros = (op1, op2) => op1 + op2;
+console.log(funcionConVariosParametros(3,7));
