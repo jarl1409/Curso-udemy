675fbdde46f365dbb83cf46feb1410df88f466bf
Jarl <jhosepandrey203@gmail.com>
2024-02-21 18:35:05 -0500

    Función setInterval en JavaScript

--- JavaScript/leccion14/14-01-FuncionesCallback-UJS.js
+++ JavaScript/leccion14/14-01-FuncionesCallback-UJS.js
@@ -1,33 +1,7 @@
-miFuncion1();
-miFuncion2();
-
-function miFuncion1(){
-    console.log('Función 1');
+let reloj = () => {
+    let fecha = new Date ();
+    console.log(`${fecha.getHours()}:${fecha.getMinutes()}:${fecha.getSeconds()}`);
 }
 
-function miFuncion2(){
-    console.log('Función 2');
-}
+setInterval(reloj, 1000); //1 seg
 
-//Función de tipo callback
-function imprimir(mensaje){
-    console.log(mensaje);
-}
-
-function sumar (op1, op2, funcionCallback){
-    let res = op1 + op2;
-    funcionCallback ( `Resultado: ${res} `);
-}
-sumar(5,3,imprimir);
-
-
-// Llamadas asíncronas con uso setTimeout
-
-function miFuncionCallback(){
-    console.log('Saludo asíncrono después de 3 seg');
-}
-setTimeout(miFuncionCallback, 3000);//Despues de 3 segundos
-
-setTimeout( function(){console.log('saludo asíncrono 2')}, 5000);
-
-setTimeout( () => console.log('saludo asíncrono 3'), 7000);
