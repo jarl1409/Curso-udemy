c7571dd666ec663e6397be3ba92cbf3a2b247a73
Jarl <jhosepandrey203@gmail.com>
2024-02-21 18:27:21 -0500

    Función setTimeout en JavaScript

--- JavaScript/leccion14/14-01-FuncionesCallback-UJS.js
+++ JavaScript/leccion14/14-01-FuncionesCallback-UJS.js
@@ -19,3 +19,15 @@ function sumar (op1, op2, funcionCallback){
     funcionCallback ( `Resultado: ${res} `);
 }
 sumar(5,3,imprimir);
+
+
+// Llamadas asíncronas con uso setTimeout
+
+function miFuncionCallback(){
+    console.log('Saludo asíncrono después de 3 seg');
+}
+setTimeout(miFuncionCallback, 3000);//Despues de 3 segundos
+
+setTimeout( function(){console.log('saludo asíncrono 2')}, 5000);
+
+setTimeout( () => console.log('saludo asíncrono 3'), 7000);
