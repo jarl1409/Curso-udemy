1a4018e2f87f49258308b87ced76f46a42ed957e
Jarl <jhosepandrey203@gmail.com>
2024-02-21 19:10:24 -0500

    Función setTimeout y Promesas en JavaScript

--- JavaScript/leccion15/15-01-Promesas-UJS.js
+++ JavaScript/leccion15/15-01-Promesas-UJS.js
@@ -11,4 +11,14 @@ let miPromesa = new Promise((resolver, rechazar) => {
     // error => console.log(error)
 // );
 
-miPromesa.then ( valor => console.log(valor)).catch(error=>console.log(error));
+// miPromesa.then ( valor => console.log(valor)).catch(error=>console.log(error));
+
+let promesa = new Promise((resolver) => {
+    console.log('Incio promesa');
+    setTimeout( ()=>resolver('Saludos con promesa y timeout'), 3000);
+    console.log('Fin promesa');
+});
+
+promesa.then(valor => console.log(valor));
+
+
