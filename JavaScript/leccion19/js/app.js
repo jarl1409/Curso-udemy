b9e4ae5398244a643017e932b02e828a460bc528
Jarl1409 <jhosepandrey203@hotmail.com>
2024-03-17 19:55:52 -0500

    Listado de Personas con JavaScript

--- JavaScript/leccion19/js/app.js
+++ JavaScript/leccion19/js/app.js
@@ -1,1 +1,14 @@
-console.log('Listado de personas');
+const personas = [
+    new Persona('Juan', 'Perez'),
+    new Persona('Karla', 'Lara')
+];
+
+function mostrarPersonas(){
+    console.log('Mostrar Personas...');
+    let texto = '';
+    for(let persona of personas){
+        console.log(persona);
+        texto += `<li>${persona.nombre} ${persona.apellido}</li>`;
+    }
+    document.getElementById('personas').innerHTML = texto;
+};
--- JavaScript/leccion19/js/persona.js
+++ JavaScript/leccion19/js/persona.js
@@ -1,2 +1,19 @@
 console.log('Clase de persona');
-
+class Persona{
+    constructor(nombre, apellido){
+        this._nombre = nombre;
+        this._apellido = apellido;
+    }
+    get nombre(){
+        return this._nombre;
+    }
+    set nombre(nombre){
+        return this._nombre = nombre;
+    }
+    get apellido(){
+        return this._apellido;
+    }
+    set apellido(apellido){
+        return this._apellido = apellido;
+    }
+}
