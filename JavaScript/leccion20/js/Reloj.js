0ddbe0d51b0e66de8292f4b2e7fc0107d8d87d3e
Jarl1409 <jhosepandrey203@hotmail.com>
2024-03-17 21:05:01 -0500

    css activador de reloj

--- JavaScript/leccion20/js/Reloj.js
+++ JavaScript/leccion20/js/Reloj.js
@@ -1,8 +1,24 @@
 const mostrarReloj = ()=>{
     let fecha = new Date();
-    let hr = fecha.getHours();
-    let min = fecha.getMinutes();
-    let seg = fecha.getSeconds();
+    let hr = formatoHora(fecha.getHours());
+    let min = formatoHora(fecha.getMinutes());
+    let seg = formatoHora(fecha.getSeconds());
     document.getElementById('hora').innerHTML = `${hr}:${min}:${seg}`;
+
+    const meses = ['ene', 'feb', 'mar', 'abr', 'may', ' jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dec'];
+    const dias =['dom', 'lun', 'mar', 'mie', 'jue', 'vie', 'sab'];
+    let diaSemana = dias[fecha.getDay()];
+    let dia = fecha.getDate();
+    let mes = meses[fecha.getMonth()];
+    let fechaTexto = `${diaSemana}, ${dia} ${mes}`;
+    document.getElementById('fecha').innerHTML = fechaTexto;
+
+    document.getElementById('contenedor').classList.toggle('animar');
 }
+const formatoHora = (hora)=>{
+    if(hora< 10)
+    hora = '0' + hora;
+    return hora;
+}
+
 setInterval(mostrarReloj, 1000);
