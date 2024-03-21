3dc43ddb91e98388de57acf60e3ec9336e3fdb87
jarl1409 <jhosepandrey203@gmail.com>
2024-03-20 21:08:37 -0500

    Creación Arreglos de Ingresos y Egresos

---
+++ JavaScript/leccion21/js/app.js
@@ -0,0 +1,17 @@
+const ingresos = [
+    new Ingreso ('salario', 2100.00),
+    new Ingreso ('Venta Coche', 1500.00)
+];
+
+const egresos = [
+    new egresos('Renta Departamento',900.00),
+    new egresos('Ropa', 400.00)
+];
+
+let cargarApp = () =>{
+    cargarCabecero();
+}
+
+let cargarCabecero = () =>{
+    
+}
